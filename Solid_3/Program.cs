96ede27d0d7310d910d8ed472bdb563843186cb8
NameUserEnter <92257494+NameUserEnter@users.noreply.github.com>
2021-12-05 18:15:18 +0300

    Hw7

---
+++ Solid_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//Порушено принцип підстановки Лісков
+abstract class Figure
+{
+    abstract public int GetArea();
+}
+
+class Rectangle : Figure
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+    override public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+class Square : Figure
+{
+    public int Side { get; set; }
+    public override int GetArea()
+    {
+        return Side * Side;
+    }
+}
+class Program
+{
+    static int Area(Figure figure)
+    {
+        return figure.GetArea();
+    }
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle();
+        rect.Width = 5;
+        rect.Height = 10;
+
+        Console.WriteLine(Area(rect));
+        //Відповідь 100? Так, спочатку сторона квадрата 5, потом 10
+        Console.ReadKey();
+    }
+}
---
+++ Solid_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+/*Даний інтерфейс поганий тим, що він включає занадто багато методів.
+ А що, якщо наш клас товарів не може мати знижок або промокодом, або для нього немає сенсу встановлювати матеріал з 
+ якого зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в кожному класі невикористовувані в ньому методи, краще 
+розбити інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати потрібні інтерфейси.
+Перепишіть, розбивши інтерфейс на декілька інтерфейсів, керуючись принципом розділення інтерфейсу. 
+Опишіть класи книжки (розмір та колір не потрібні, але потрібна ціна та знижки) та верхній одяг (колір, розмір, ціна знижка),
+які реалізують притаманні їм інтерфейси.*/
+
+interface IPrice
+{
+    void SetPrice(double price);
+}
+interface IDiscount
+{
+    void ApplyDiscount(String discount);
+    void ApplyPromocode(String promocode);
+}
+interface IColor
+{
+    void SetColor(byte color);
+}
+interface ISize
+{
+    void SetSize(byte size);
+}
+
+class Book : IPrice, IDiscount
+{
+    private double price;
+    public void ApplyDiscount(string discount) { }
+    public void ApplyPromocode(string promocode) { }
+    public void SetPrice(double price)
+    {
+        this.price = price;
+    }
+}
+
+class Outerwear : IColor, ISize, IPrice, IDiscount
+{
+    private byte color;
+    private byte size;
+    private double price;
+    public void ApplyDiscount(string discount) { }
+    public void ApplyPromocode(string promocode) { }
+    public void SetColor(byte color)
+    {
+        this.color = color;
+    }
+    public void SetPrice(double price)
+    {
+        this.price = price;
+    }
+    public void SetSize(byte size)
+    {
+        this.size = size;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadKey();
+    }
+}
